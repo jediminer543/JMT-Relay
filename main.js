23af996d618187f31557edfe08e569297f0d9e67
jediminer543 <jediminer543@users.noreply.github.com>
2015-07-27 12:21:17 +0100

    Fanciest of Work-In-Progress GUIs

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ var BrowserWindow = require('browser-window');
 var ipc = require('ipc');
 
 var mainWindow = null;
+var licenceWindow = null;
+var optionsWindow = null;
 var subWindows = [];
 
 var opening = false;
@@ -14,9 +16,20 @@ function openPage (file) {
 	mainWindow.loadUrl('file://' + __dirname + '/' + file);
 }
 
-ipc.on("openPage", function(event, arg) {
+ipc.on("openMainPage", function(event, arg) {
 	event.sender.send("return", "loading");
 	openPage(arg);
+});
+
+ipc.on("requestMainClose", function(event, arg) {
+	event.sender.send("return", "closing");
+	mainWindow.close();
+	mainWindow = null;
+});
+
+ipc.on("requestLicence", function(event, arg) {
+	event.sender.send("return", "licenceing");
+	if (!licenceWindow.isVisible()) { licenceWindow.show(); } else { licenceWindow.hide(); }
 });
 
 app.on('window-all-closed', function() {
@@ -26,7 +39,7 @@ app.on('window-all-closed', function() {
 });
 
 app.on('ready', function() {
-	mainWindow = new BrowserWindow({width: 800, height: 600, frame: true, resizable:false});
+	mainWindow = new BrowserWindow({width: 800, height: 600, frame: false, resizable:false});
 	
 	openPage("index.html");
 	
@@ -35,8 +48,8 @@ app.on('ready', function() {
 	    mainWindow = null;
 	});
 	
-	mainWindow.onbeforeunload = function (e) {
-		
-		return false;
-	};
+	licenceWindow = new BrowserWindow({width: 640, height: 480, frame: false, resizable:false, show:false});
+	licenceWindow.loadUrl('file://' + __dirname + '/' + "licence.html");
+	
+	optionsWindow = new BrowserWindow({width: 800, height: 700, frame: false, resizable:false, show:false});
 });
